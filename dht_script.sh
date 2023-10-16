d69a5c3e85ecc3dc264f00c283a921fdb09e4a2d
nicksccs <140536382+nicksccs@users.noreply.github.com>
2023-10-16 14:01:36 -0700

    Update dht_script.sh

--- dht_script.sh
+++ dht_script.sh
@@ -28,7 +28,70 @@ sudo dpkg-reconfigure --priority=low unattended-upgrades
 cat <<EOL > /opt/sensor-metrics/sensor.py
 #!/usr/bin/env python3
 
-# ... (Your Python script content)
+import logging
+import time
+
+import Adafruit_DHT
+
+from prometheus_client import Gauge, start_http_server
+from systemd.journal import JournalHandler
+
+# Setup logging to the Systemd Journal
+log = logging.getLogger('dht22_sensor')
+log.addHandler(JournalHandler())
+log.setLevel(logging.INFO)
+
+# Initialize the DHT11 sensor
+# Read data from GPIO4 pin on the Raspberry Pi
+SENSOR = Adafruit_DHT.DHT22
+SENSOR_PIN = 17
+
+# The time in seconds between sensor reads
+READ_INTERVAL = 60
+
+# Create Prometheus gauges for humidity and temperature in
+# Celsius and Fahrenheit
+gh = Gauge('DO_Server_humidity_percent',
+           'Humidity percentage measured by the DHT22 Sensor')
+gt = Gauge('DO_Server_temperature',
+           'Temperature measured by the DHT22 Sensor', ['scale'])
+
+# Initialize the labels for the temperature scale
+gt.labels('celsius')
+gt.labels('fahrenheit')
+
+def celsius_to_fahrenheit(degrees_celsius):
+        return (degrees_celsius * 9/5) + 32
+
+def read_sensor():
+    try:
+        humidity, temperature = Adafruit_DHT.read_retry(SENSOR, SENSOR_PIN)
+    except RuntimeError as e:
+        # GPIO access may require sudo permissions
+        # Other RuntimeError exceptions may occur, but
+        # are common.  Just try again.
+        log.error("RuntimeError: {}".format(e))
+
+    if humidity is not None and temperature is not None:
+        gh.set(humidity)
+        gt.labels('celsius').set(temperature)
+        gt.labels('fahrenheit').set(celsius_to_fahrenheit(temperature))
+
+        log.info("Temp:{0:0.1f}*C, Humidity: {1:0.1f}%".format(temperature, humidity))
+
+    time.sleep(READ_INTERVAL)
+
+if __name__ == "__main__":
+    # Expose metrics
+    metrics_port = 8000
+    start_http_server(metrics_port)
+    print("Serving sensor metrics on :{}".format(metrics_port))
+    log.info("Serving sensor metrics on :{}".format(metrics_port))
+
+    while True:
+        read_sensor()
+
+
 
 EOL
 
